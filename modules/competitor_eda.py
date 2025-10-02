63a5c55d443b1e239ce10b2c32f3f009f1e02ecd
Sarang C Santhosh <sarang.santhosh@mba.christuniversity.in>
2025-10-02 19:06:09 +0530

    Update competitor_eda.py

--- modules/competitor_eda.py
+++ modules/competitor_eda.py
@@ -127,7 +127,7 @@ def run():
     # --------------------------
     # TABS
     # --------------------------
-    tab1, tab2, tab3 = st.tabs([
+    tab1, tab2, tab3, tab4 = st.tabs([
         "Brand Distribution",
         "Pack Size Wise Analysis",
         "Top SKUs",
