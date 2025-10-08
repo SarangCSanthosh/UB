e38db3a7fee54b5561919955dd54d0aa1a5a0704
shreyaramesh-code <shreya.ramesh@mba.christuniversity.in>
2025-10-08 12:02:21 +0530

    Update primary_eda.py

--- modules/primary_eda.py
+++ modules/primary_eda.py
@@ -209,9 +209,10 @@ def run():
         st.markdown("""
 ### **Insights: Shipment Volume Analysis (2023–2024)**
 
-- There has been a substantial decrease in its yearly shipment volume between 2023 and 2024. This indicates a contraction in shipping activity or demand.
+- There has been a substantial decrease in the yearly shipment volume between 2023 and 2024. This indicates a contraction in shipping activity or demand.
 - The company's shipment activity is highly cyclical, with a common drop observed in Q3 of both years (though much more severe in 2024). The period from 2024 Q2 to 2024 Q3 represents a major performance concern due to the unprecedented low volume.
-- The shipment volume is highly unstable on a monthly basis. While the first half of 2024 contained the single highest month (May), the subsequent crash to the lowest volume in August suggests a major disruption, either due to internal capacity issues, a massive short-term demand spike followed by exhaustion, or an abrupt external market shock. The recent recovery in Q4 2024 (September and October) is a positive sign, but volume remains far below peak levels.
+- The shipment volume is highly unstable on a monthly basis. While the first half of 2024 contained the single highest month (May), the subsequent crash to the lowest volume in August. The recent recovery in Q4 2024 (September and October) is a positive sign, but volume remains far below peak levels.
+
 """)
 
 
