fb6b5695e82fdd14d940a246b12770a22274b4e0
Sarang C Santhosh <sarang.santhosh@mba.christuniversity.in>
2025-10-16 09:23:32 +0530

    Update secondary_eda.py

--- modules/secondary_eda.py
+++ modules/secondary_eda.py
@@ -47,8 +47,9 @@ def load_event_calendar(sheet_id: str):
     """
     try:
         # Build a direct download link
-        download_url = f"https://docs.google.com/spreadsheets/d/1QYN4ZHmB-FpA1wUFlzh5Vp-WtMFPV8jO/export?format=xlsx"
-
+        
+        #download_url = f"https://docs.google.com/spreadsheets/d/1QYN4ZHmB-FpA1wUFlzh5Vp-WtMFPV8jO/export?format=xlsx"
+        download_url = f"https://docs.google.com/spreadsheets/d/1PZSyJWB_1iPbARkUOiNOVooF51PjDhxlgGxgCdSCzKk/export?format=xlsx"
         df = pd.read_excel(download_url)
 
         # Clean and standardize columns
