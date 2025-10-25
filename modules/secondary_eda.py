111e7e4e766c20c65c000982c641fb806ea77850
Sarang C Santhosh <sarang.santhosh@mba.christuniversity.in>
2025-10-25 12:32:42 +0530

    Update secondary_eda.py

--- modules/secondary_eda.py
+++ modules/secondary_eda.py
@@ -143,10 +143,10 @@ def run():
     # --------------------------
     # SHOW FIXED KPIs
     # --------------------------
-    col1, col2, col3 = st.columns(3)
+    col1, col2 = st.columns(2)
     col1.metric("Total Volume", f"{int(kpi_volume):,}", format_delta(delta_volume))
-    col2.metric("Unique Outlets", f"{kpi_outlets}", format_delta(delta_outlets))
-    col3.metric("Total Shipments", f"{kpi_shipments}", format_delta(delta_shipments))
+    #col2.metric("Unique Outlets", f"{kpi_outlets}", format_delta(delta_outlets))
+    col2.metric("Total Shipments", f"{kpi_shipments}", format_delta(delta_shipments))
     st.caption(f"YoY change: {latest_year} vs {prev_year}")
     st.markdown("---")
 
