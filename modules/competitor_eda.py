2f2f4e8c3a92f4c91967d1703f85a3d686ea2894
Sarang C Santhosh <sarang.santhosh@mba.christuniversity.in>
2025-11-04 21:09:16 +0530

    Update competitor_eda.py

--- modules/competitor_eda.py
+++ modules/competitor_eda.py
@@ -55,7 +55,7 @@ def run():
 
     df, DATE_COL = prepare_dates(df)
     # Map SKU → Brand before any tab uses it
-    df["Brand"] = df[SKU_COL].apply(map_sku_to_brand)
+    #df["Brand"] = df[SKU_COL].apply(map_sku_to_brand)
 
 
     # --------------------------
