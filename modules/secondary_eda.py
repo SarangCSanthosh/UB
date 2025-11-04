43fa055bb20509268df1506827290b6daeba20fd
Sarang C Santhosh <sarang.santhosh@mba.christuniversity.in>
2025-11-04 20:28:57 +0530

    Update secondary_eda.py

--- modules/secondary_eda.py
+++ modules/secondary_eda.py
@@ -348,7 +348,7 @@ def run():
             # ------------------------
             # RIGHT: Bubble Chart of Event Bins
             # ------------------------
-            with col2:
+	        with col2:
                 st.subheader("Event Type Distribution")
             
                 bin_cols = ["Political", "Festival", "Sports", "Celebrity_Deaths", "Public_Holiday", "Movie", "Weekend"]
