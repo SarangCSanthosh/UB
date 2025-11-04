96101e1e86a0510eb6e5f94c9ae35c623453a6f4
Sarang C Santhosh <sarang.santhosh@mba.christuniversity.in>
2025-11-04 21:08:46 +0530

    Update competitor_eda.py

--- modules/competitor_eda.py
+++ modules/competitor_eda.py
@@ -25,49 +25,7 @@ def prepare_dates(df, date_col="ACTUAL_DATE"):
 # ===============================
 # Exact SKU to Brand mapping
 # ===============================
-SKU_GROUP_MAPPING = {
-    # KF
-    "KF 330 ML.": "KF LAGER",
-    "KF 330 ML. CANS": "KF LAGER",
-    "KF 500 ML. CANS": "KF LAGER",
-    "KF 650 ML.": "KF LAGER",
 
-    # KF STORM
-    "KF STORM 500 ML. CANS": "KF STORM",
-    "KF STORM 650 ML.": "KF STORM",
-
-    # KF ULTRA
-    "KF ULTRA 330 ML.": "KF ULTRA",
-    "KF ULTRA 500 ML. CAN": "KF ULTRA",
-    "KF ULTRA 650 ML.": "KF ULTRA",
-
-    # KF ULTRA MAX
-    "KF ULTRA MAX 330 ML.": "KF ULTRA MAX",
-    "KF ULTRA MAX 500 ML. CANS": "KF ULTRA MAX",
-    "KF ULTRA MAX 650 ML.": "KF ULTRA MAX",
-
-    # KF ULTRA WITBIER
-    "KF ULTRA WITBIER 330 ML.": "KF ULTRA WITBIER",
-    "KF ULTRA WITBIER 500 ML. CANS": "KF ULTRA WITBIER",
-    "KF ULTRA WITBIER 650 ML.": "KF ULTRA WITBIER",
-
-    # KFS
-    "KFS 330 ML.": "KF STRONG",
-    "KFS 330 ML. CANS": "KF STRONG",
-    "KFS 500 ML. CANS": "KF STRONG",
-    "KFS 650 ML.": "KF STRONG",
-
-    # Bullet 
-    "BSSB 300 ML.": "Bullet", 
-    "BSSB 330 ML.": "Bullet", 
-    "BSSB 330 ML. CANS": "Bullet", 
-    "BSSB 650 ML.": "Bullet",
-
-    
-}
-
-def map_sku_to_brand(sku):
-    return SKU_GROUP_MAPPING.get(sku.strip().upper(), "OTHER")
 
 # ===============================
 # Main app
